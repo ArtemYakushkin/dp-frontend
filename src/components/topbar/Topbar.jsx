50fd1dc68213b73e9f9d138538a71eee150fc54c
ArtemYakushkin <zimberger29101985@gmail.com>
2023-03-11 02:27:58 +0200

    add page home

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { IoSearchSharp, IoPersonSharp, IoNotifications, IoChatbox } from "react-icons/io5";
+import './topbar.css';
+import Stark from '../../assets/person/stark.jpg';
+
+export const Topbar = () => {
+    return (
+        <div className="topbarContainer">
+            <div className="container">
+                <div className="topbarWrapper">
+                    <div className="topbarLeft">
+                        <span className="logo">Dear Penfriend</span>
+                    </div>
+
+                    <div className="topbarCenter">
+                        <div className="searchbar">
+                            <IoSearchSharp className="searchIcon" />
+                            <input className="searchInput" placeholder="Search for..." />
+                        </div>
+                    </div>
+
+                    <div className="topbarRight">
+                        <div className="topbarLinks">
+                            <span className="topbarLink">Home</span>
+                            <span className="topbarLink">Timeline</span>
+                        </div>
+                        <div className="topbarIcons">
+                            <div className="topIconItem">
+                                <IoPersonSharp className="topIcon" />
+                                <span className="topbarIconBage">1</span>
+                            </div>
+                            <div className="topIconItem">
+                                <IoChatbox className="topIcon" />
+                                <span className="topbarIconBage">2</span>
+                            </div>
+                            <div className="topIconItem">
+                                <IoNotifications className="topIcon" />
+                                <span className="topbarIconBage">1</span>
+                            </div>
+                        </div>
+                        <img src={Stark} alt="" className="topbarImg" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import { Topbar } from "components/topbar/Topbar";
+import React from "react";
+
+export const Home = () => {
+    return (
+        <div>
+            <Topbar/>
+        </div>
+    )
+}
